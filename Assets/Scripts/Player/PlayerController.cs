a59ad5633af42cf3703c49a36007edd07369395a
Eric Liu <eeric048@uottawa.ca>
2026-01-31 01:10:54 -0500

    dynamic bounds according to screensize

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,16 +6,15 @@ namespace Player
     {
         [SerializeField] private float normalSpeed = 400.0f;
         [SerializeField] private float focusSpeed = 160.0f;
-        [SerializeField] public float xBound = 600.0f;
-        [SerializeField] public float yBound = 720.0f;
-
+        // [SerializeField] public float xBound = 600.0f;
+        // [SerializeField] public float yBound = 720.0f;
         private float _screenHeight;
         private float _screenWidth;
-
+        
         private float _FOPheight;
         private float _FOPwidth;
         
-        private float FOPaspectRatio = 730f/850f; // the ratio of 730/850 (H/W) needs to be maintained for the field of play
+        private readonly float _targetAspectRatio = 730f/850f; // the ratio of 730/850 needs to be maintained for the field of play
 
 
         [SerializeField] private GameObject hitBoxSprite;
@@ -26,13 +25,28 @@ namespace Player
             _isHitBoxSpriteNotNull = hitBoxSprite != null;
             _screenHeight = Screen.height/2;
             _screenWidth = Screen.width/2;
+            CalculateBoundaries();
 
-            _FOPheight = _screenHeight*0.93f*2;
             
-            _FOPwidth = 1f/FOPaspectRatio*_FOPheight;
-            Debug.Log(_FOPheight);
-            Debug.Log(_FOPheight);
-            
+        }
+
+        //method to calculate the FOP depending on screensize while maintaining aspect ratio
+        private void CalculateBoundaries()
+        {
+            float currentScreenRatio = _screenHeight / _screenWidth;
+
+            if (currentScreenRatio > _targetAspectRatio)
+            {
+                // Screen is too tall: Width is the constraint
+                _FOPwidth = _screenWidth*0.45f;
+                _FOPheight = _FOPwidth * _targetAspectRatio;
+            }
+            else
+            {
+                // Screen is too wide: Height is the constraint
+                _FOPheight = _screenHeight*0.93f;
+                _FOPwidth = _FOPheight / _targetAspectRatio;
+            }
         }
 
         private void Update()
@@ -58,6 +72,7 @@ namespace Player
 
             Vector3 displacement = inputDir * (curSpeed * Time.deltaTime);
             var newPos = transform.position + displacement;
+
 
             //clamping character movement
             var clampedX = Mathf.Clamp(newPos.x, -_FOPwidth, _FOPwidth);
