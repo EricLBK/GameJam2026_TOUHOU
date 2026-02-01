1c4018ecb7b6f726e4a05650c07d9c1085ab5165
Jonathan Cao <caojiasen@gmail.com>
2026-02-01 09:39:55 -0800

    Make stopping SpawnPattern optional

--- Assets/Scripts/Bullets/BulletManager.cs
+++ Assets/Scripts/Bullets/BulletManager.cs
@@ -203,9 +203,14 @@ namespace Bullets
             StartCoroutine(moveBulletWhileActive());
         }
 
-        public void SpawnPattern(BulletPattern pattern, float2 initPosition, float duration)
+        // duration of 0 means run forever
+        public void SpawnPattern(BulletPattern pattern, float2 initPosition, float duration = 0)
         {
             var handle = StartCoroutine(pattern(this, initPosition: initPosition));
+
+            if (duration == 0) {
+                return;
+            }
 
             IEnumerator stopPattern()
             {
@@ -216,11 +221,11 @@ namespace Bullets
             StartCoroutine(stopPattern());
         }
 
-        public void SpawnPattern(BulletPattern[] patterns, float2 initPosition, float duration)
+        public void SpawnPattern(BulletPattern[] patterns, float2 initPosition, float duration = 0)
         {
             foreach (var pattern in patterns)
             {
-                SpawnPattern(pattern, initPosition, duration);
+                SpawnPattern(pattern, initPosition, duration: duration);
             }
         }
 
