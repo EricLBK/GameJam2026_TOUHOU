9fe25ff13f98a488e1436622f60e24b95a68b659
NicholasMa01 <nicholasmamusic@gmail.com>
2026-02-01 12:44:41 -0500

    Added Sound Effects Audio Wwise

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_ENEMYBULLET = 863241602U;
+        static const AkUniqueID PLAY_ENEMYDEATH = 3154146856U;
+        static const AkUniqueID PLAY_ENEMYHIT = 2534390857U;
+        static const AkUniqueID PLAY_MENUCLICKHOVER = 3016872785U;
+        static const AkUniqueID PLAY_PLAYERDAMAGE = 1014941850U;
+        static const AkUniqueID PLAY_PLAYERDEATH = 910581297U;
+        static const AkUniqueID PLAY_PLAYERFOCUSTOGGLE = 1297444699U;
+        static const AkUniqueID PLAY_PLAYERPICKUPITEM = 3103906424U;
+        static const AkUniqueID PLAY_PLAYERSHOOTFOCUS = 2769748522U;
+        static const AkUniqueID PLAY_PLAYERSHOOTNORMAL = 7909091U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAINSOUNDBANK = 534561221U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
