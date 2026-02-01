db5567be1b2fdc1e608858b63f277c6d93aa858f
Locrium <112211021+Locrium@users.noreply.github.com>
2026-02-01 08:38:55 -0500

    lost everything in a crash

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class MainMenuUI : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("MainScene");
+        SceneManager.LoadSceneAsync("NormalPlaythrough");
     }
     public void DemoGame()
     {
